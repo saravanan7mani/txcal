40e283c2e1699905c53ad83bfd69a1be73854b49
Saravanan Mani <samani2@cisco.com>
2022-03-23 17:13:46 +0530

    final part cleaned

--- app.js
+++ app.js
@@ -18,61 +18,84 @@ const block_number = 680000;
 
 calculate(block_number)
 
+async function getBlockHash(blocknumber) {
+  if (!Number.isSafeInteger(blocknumber)) {
+    throw new Error ('Invalid block number ' + blocknumber)
+  }
+  const block_height_response = await axios.get(url + '/block-height/' + blocknumber);
+
+  if (block_height_response.status !== 200) {
+    throw new Error ('Invalid http status code for get block hash request ' + block_height_response.status)
+  }
+
+  if (block_height_response.data == null) {
+    throw new Error ('Invalid response from get block hash request for block number ' + blocknumber)
+  }
+
+  return block_height_response.data;
+}
+
+async function getTxCount(block_hash) {
+  const block_response = await axios.get(url + '/block/' + block_hash);
+
+  if (block_response.status !== 200) {
+    throw new Error ('Invalid http status code for get tx count request ' + block_response.status)
+  }
+
+  if (block_response.data == null || block_response.data.tx_count == null) {
+    throw new Error ('Invalid response from get tx count request for block hash ' + block_hash)
+  }
+
+  return block_response.data.tx_count
+}
+
+async function  getTxs(block_hash, i) {
+  const txs_response = await axios.get(url + '/block/' + block_hash + '/txs/' + i * 25);
+
+  if (txs_response.status !== 200) {
+    throw new Error ('Invalid http status code ' + txs_response.status)
+  }
+
+  if (txs_response.data == null) {
+    throw new Error ('Invalid txs response for ' + txs_response)
+  }
+
+  return txs_response.data;
+}
+
+
+async function populateMap(block_hash, tx_count) {
+  const loop_count = (~~(tx_count / 25)) + ((tx_count % 25) ? 1 : 0);
+  
+  console.log('tx_count ' + tx_count);
+  console.log('loop_count ' + loop_count);
+
+  const txid_map = new Map();
+
+  for (let i = 0; i < loop_count; i++) {
+    const txs_data = await getTxs(block_hash, i);
+
+    txs_data.forEach(function(txid) { 
+      // console.log('txid' + txid.txid);
+      const txid_info = {vin_txids: [], ansc_count: 0, visited: false};
+      txid_map.set(txid.txid, txid_info)
+      txid.vin.forEach(function(vin_txid) {
+        // console.log('txid ' + txid.txid +  ', vin_txid ' + vin_txid.txid);
+        txid_info.vin_txids.push(vin_txid.txid);
+      });
+    });
+  }
+
+  return txid_map;
+}
+
 async function calculate(blocknumber) {
   try {
-    const block_height_response = await axios.get(url + '/block-height/' + blocknumber);
+    const block_hash = await getBlockHash(blocknumber);
+    const tx_count = await getTxCount(block_hash);
+    const txid_map = await populateMap(block_hash, tx_count);
 
-    if (block_height_response.status !== 200) {
-      throw new Error ('Invalid http status code ' + block_height_response.status)
-    }
-
-    if (block_height_response.data == null) {
-      throw new Error ('Invalid input block number code ' + blocknumber)
-    }
-
-    const block_hash = block_height_response.data;
-
-    const block_response = await axios.get(url + '/block/' + block_hash);
-
-    if (block_response.status !== 200) {
-      throw new Error ('Invalid http status code ' + block_response.status)
-    }
-
-    if (block_response.data == null || block_response.data.tx_count == null) {
-      throw new Error ('Invalid block hash ' + block_response)
-    }
-
-    const tx_count = block_response.data.tx_count
-    
-    const loop_count = ~~(tx_count / 25) + (tx_count % 25) ? 1 : 0;
-
-    const txid_map = new Map();
-
-    for (let i = 0; i < loop_count; i++) {
-      const txs_response = await axios.get(url + '/block/' + block_hash + '/txs/' + i * 25);
-
-      if (txs_response.status !== 200) {
-        throw new Error ('Invalid http status code ' + txs_response.status)
-      }
-
-      if (txs_response.data == null) {
-        throw new Error ('Invalid txs response for ' + txs_response)
-      }
-
-      const txs_data = txs_response.data;
-
-      txs_data.forEach(function(txid) { 
-        // console.log('txid' + txid.txid);
-        const txid_info = {vin_txids: [], ansc_count: 0, visited: false};
-        txid_map.set(txid.txid, txid_info)
-        txid.vin.forEach(function(vin_txid) {
-          // console.log('txid ' + txid.txid +  ', vin_txid ' + vin_txid.txid);
-          txid_info.vin_txids.push(vin_txid.txid);
-        });
-      });
-    }
-
-    // console.log(txid_map.size);
+    console.log('map size' + txid_map.size);
 
     process(txid_map);
 
@@ -83,14 +106,14 @@ async function calculate(blocknumber) {
 }
 
 function process(txid_map) {
-  console.log('process ' + txid_map);
+  // console.log('process ' + txid_map);
   const large_txs = [];
   txid_map.forEach((txid_info, txid) => {
     if (txid_info.visited) {
       return;
     }
 
-    console.log('process 2 ' + txid_map);
+    // console.log('process 2 ' + txid_map);
     const txsize = calcAnscSize(txid, txid_map);
     large_txs.push( {tx_size: txsize, tx_id: txid});
     large_txs.sort((a, b) => {
@@ -104,7 +127,7 @@ function process(txid_map) {
 }
 
 function calcAnscSize(txid, txid_map) {
-  console.log('calcAnscSize ' + txid_map);
+  // console.log('calcAnscSize ' + txid_map);
   if (txid_map.has(txid)) {
     const txid_info = txid_map.get(txid);
     if (txid_info.visited) {
