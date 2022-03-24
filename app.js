a87fcd5cdeef8e1b803b3d5f11e2bef0c8e09ec5
Saravanan Mani <samani2@cisco.com>
2022-03-24 13:54:54 +0530

    improving performance with concurrent get txs http requests and rate limiting

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const {getBlockHash} = require('./block_explorer_service');
 const {getTxCount} = require('./block_explorer_service');
 const {getTxs} = require('./block_explorer_service');
 
+const RATE_LIMIT = 100;
+
 calculate()
 
 async function calculate() {
@@ -32,20 +34,28 @@ async function populateMap(block_hash, tx_count) {
   const loop_count = (~~(tx_count / 25)) + ((tx_count % 25) ? 1 : 0);
 
   const txid_map = new Map();
+  const get_txs_promises = [];
+  for (let i = 1; i <= loop_count; i++) {
+    get_txs_promises.push(getTxs(block_hash, i-1));
 
-  for (let i = 0; i < loop_count; i++) {
-    const txs_data = await getTxs(block_hash, i);
-    
-    txs_data.forEach(function(txid) { 
-      const vin_txid_set = new Set();
+    if (i%RATE_LIMIT === 0 || i === loop_count) {
+      const txs_datas = await Promise.all(get_txs_promises);
 
-      const txid_info = {vin_txids: [], ansc_count: 0, visited: false};
-      txid_map.set(txid.txid, txid_info)
-      txid.vin.forEach(function(vin_txid) {
-        vin_txid_set.add(vin_txid.txid);
-      });
-      txid_info.vin_txids = [...vin_txid_set];
-    });
+      Promise.resolve(txs_datas).then((txs_datas) => {
+        txs_datas.forEach((txs_data) => {
+          txs_data.forEach(function(txid) {
+            const vin_txid_set = new Set();
+      
+            const txid_info = {vin_txids: [], ansc_count: 0, visited: false};
+            txid_map.set(txid.txid, txid_info)
+            txid.vin.forEach(function(vin_txid) {
+              vin_txid_set.add(vin_txid.txid);
+            });
+            txid_info.vin_txids = [...vin_txid_set];
+          });
+        })
+      })
+    }
   }
 
   return txid_map;
